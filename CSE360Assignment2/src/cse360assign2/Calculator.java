6fc14cb42e8146387cf966b7c96f3d05d8ca2d7a
Will Nobles <wnobles@asu.edu>
2019-02-21 10:21:31 -0700

    Initial version includes Javadoc comments

---
+++ CSE360Assignment2/src/cse360assign2/Calculator.java
@@ -0,0 +1,67 @@
+/**
+ * This class performs simple mathematical calculations.
+ * 
+ * @author - Will G. Nobles
+ * Class ID - 511
+ */
+
+package cse360assign2;
+
+public class Calculator {
+	private int total;
+
+	/**
+	 * Creates a calculator with an initial total of 0
+	 */
+	public Calculator() {
+		total = 0; // not needed - included for clarity
+	}
+
+	/**
+	 * Returns the total calculated
+	 * @return total
+	 */
+	public int getTotal() {
+		return 0;
+	}
+
+	/**
+	 * Adds a value to the total
+	 * @param value
+	 */
+	public void add(int value) {
+
+	}
+
+	/**
+	 * Subtracts a value from the total
+	 * @param value
+	 */
+	public void subtract(int value) {
+
+	}
+
+	/**
+	 * Multiplies the total by a value
+	 * @param value
+	 */
+	public void multiply(int value) {
+
+	}
+
+	/**
+	 * Divides the total by a value
+	 * @param value
+	 */
+	public void divide(int value) {
+
+	}
+
+	/**
+	 * Returns all calculations completed
+	 * @return
+	 */
+	public String getHistory() {
+		return "";
+	}
+}
